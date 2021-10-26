d57a9c7e451fc8874c33aa56d245e2ead7bb6ed4
artryan130 <80287857+artryan130@users.noreply.github.com>
2021-10-26 20:15:23 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ALoo
+          Learn React ok
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App;;
