ba5d8a48af794907759ed514bef5b1f09683ff2d
artryan130 <80287857+artryan130@users.noreply.github.com>
2021-10-26 21:20:49 -0300

    Cadastro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Cadastro from './componentes/cadastro';
+import Cadastro from './componentes/cadastro/cadastro.js';
 
 function App() {
   return (
