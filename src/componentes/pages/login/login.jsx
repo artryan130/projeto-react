ba26fdab85ef2d1fc122f351d6804545c868d961
artryan130 <80287857+artryan130@users.noreply.github.com>
2021-11-12 11:33:15 -0300

    Consumindo API

--- src/componentes/pages/login/login.jsx
+++ src/componentes/pages/login/login.jsx
@@ -2,31 +2,54 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react";
 import './login.css';
+import { useHistory } from "react-router";
 
 const Login = () =>{
+    
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const history = useHistory();
+
+    function handleEmailChange(event){
+        setEmail(event.target.value);
+    }
+
+    function handlePasswordChange(event){
+        setPassword(event.target.value);
+    }
+    
+    function handleSubmit(event) {
+        console.log("Enviando form");
+        event.preventDefault();
+        axios.post('https://webapp290016.ip-198-58-124-158.cloudezapp.io/users/login', {email, password})
+        .then((res) => history.push("/"))
+        .catch((err) => console.log(err));
+    }
+    
     return (
         <div className="login">
+            <form method="POST" onSubmit={handleSubmit}>
+                <div className="l-text">
+                    <h1>Login</h1>
+                </div>
 
-            <div className="l-text">
-                <h1>Login</h1>
-            </div>
+                <div className="l-line1">
+                    <input className="l-email" type="email" onChange={handleEmailChange} placeholder="Email"/>
+                </div>
 
-            <div className="l-line1">
-                <input className="l-email" type="email" placeholder="Email"/>
-            </div>
+                <div className="l-line2">
+                    <input className="l-password" type="password" onChange={handlePasswordChange} placeholder="Password"/>
+                </div>
 
-            <div className="l-line2">
-                <input className="l-password" type="password" placeholder="Password"/>
-            </div>
-
-            <div>
-                <button type="submit" className="l-button">Entrar</button>
-            </div>
-            
-            <div className="l-register">
-                <h2>Não tem uma conta ainda? <a href="/register">Cadastre-se</a></h2>
-            </div>
-
+                <div>
+                    <button type="submit" className="l-button">Entrar</button>
+                </div>
+                
+                <div className="l-register">
+                    <h2>Não tem uma conta ainda? <a href="/register">Cadastre-se</a></h2>
+                </div>
+            </form>
         </div>
     )
 };
--- src/componentes/pages/register/register.jsx
+++ src/componentes/pages/register/register.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react";
 import './register.css';
+import { useHistory } from "react-router";
 
 const Register = () =>{
     const [name, setName] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+
+    const history = useHistory();
 
     function handleNameChange(event) {
         setName(event.target.value);
@@ -20,18 +23,17 @@ const Register = () =>{
         setPassword(event.target.value);
     }
 
-    // function handleSubmit(event) {
-    //     console.log("Enviar formulario");
-    //     event.preventDefault();
-    //     axios
-    //         .post("/users", { name, email, password })
-    //         .then((res) => history.push("/"))
-    //         .catch((err) => console.log(err));
-    // }
+    function handleSubmit(event) {
+        console.log("Enviar formulario");
+        event.preventDefault();
+        axios.post('https://webapp290016.ip-198-58-124-158.cloudezapp.io/users', { name, email, password })
+            .then((res) => history.push("/"))
+            .catch((err) => console.log(err));
+    }
 
     return (
         <div className="register">
-            {/* <form method="POST" onSubmit={handleSubmit}> */}
+            <form method="POST" onSubmit={handleSubmit}>
 
                 <div className="r-arrow">
                     <a href="/"><svg xmlns="http://www.w3.org/2000/svg" height="64px" viewBox="0 0 24 24" width="64px" fill="#1D3557"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg></a>
@@ -53,7 +55,7 @@ const Register = () =>{
 
                 <button type="submit" className="r-button">Confirmar</button>
                 
-            {/* </form> */}
+            </form>
         </div>
     )
 }
