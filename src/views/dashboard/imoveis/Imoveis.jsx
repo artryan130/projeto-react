a5ac837dfdca0618b0d9998bdeeb68aee84c35ae
André Luiz Alves Costa <andrelac963@gmail.com>
2021-11-25 10:32:07 -0300

    posições

--- src/views/dashboard/imoveis/Imoveis.jsx
+++ src/views/dashboard/imoveis/Imoveis.jsx
@@ -1,9 +1,15 @@
-import './Imoveis.css'
+import "./Imoveis.css";
 
 export default function Imoveis() {
-    return(
-        <div>
-
-        </div>
-    )
+  return (
+    <div className="Imoveis">
+      <div className="Title">
+        <h1>Imóveis</h1>
+        <input placeholder="Pesquisar" className="Pesquisar"></input>
+      </div>
+      <div className="Content">
+        <h1>teste</h1>
+      </div>
+    </div>
+  );
 }
