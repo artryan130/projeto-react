9d81ff99c3ebc0e98d6b3d28514413155fc56de3
André Luiz Alves Costa <andrelac963@gmail.com>
2021-11-25 09:35:33 -0300

    sidebar

--- src/views/dashboard/sidebar/Sidebar.jsx
+++ src/views/dashboard/sidebar/Sidebar.jsx
@@ -3,36 +3,42 @@ import { NavLink } from "react-router-dom";
 import Home from "../../../assets/home.svg";
 import Add from "../../../assets/add.svg";
 import User from "../../../assets/user.svg";
+import Logout from "../../../assets/logout.svg";
 
 import "./Sidebar.css";
 
 export default function Sidebar() {
   return (
     <nav class="navbar">
-      <form class="form-inline">
-        <NavLink
-          to="/dashboard/imoveis"
-          exact
-          activeClassName="selected"
-          className="NavbarItem"
-        >
-          <img src={Home} alt="Imoveis" className="NavItemLogo" />
-        </NavLink>
-        <NavLink
-          to="/dashboard/newimoveis"
-          activeClassName="selected"
-          className="NavbarItem"
-        >
-          <img src={Add} alt="NewImoveis" className="NavItemLogo" />
-        </NavLink>
-        <NavLink
-          to="/dashboard/userlist"
-          activeClassName="selected"
-          className="NavbarItem"
-        >
-          <img src={User} alt="Listar Usuários" className="NavItemLogo" />
-        </NavLink>
-      </form>
+      <NavLink
+        to="/dashboard/imoveis"
+        exact
+        activeClassName="selected"
+        className="NavbarItem"
+      >
+        <img src={Home} alt="Imoveis" className="NavItemLogo1" />
+      </NavLink>
+      <NavLink
+        to="/dashboard/newimoveis"
+        activeClassName="selected"
+        className="NavbarItem"
+      >
+        <img src={Add} alt="NewImoveis" className="NavItemLogo" />
+      </NavLink>
+      <NavLink
+        to="/dashboard/userlist"
+        activeClassName="selected"
+        className="NavbarItem"
+      >
+        <img src={User} alt="Listar Usuários" className="NavItemLogo" />
+      </NavLink>
+      <NavLink
+        to="/"
+        activeClassName="selected"
+        className="NavbarItem"
+      >
+        <img src={Logout} alt="Sair" className="NavItemLogo" />
+      </NavLink>
     </nav>
   );
 }
