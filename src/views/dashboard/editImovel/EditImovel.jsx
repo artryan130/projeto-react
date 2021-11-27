165680957faa5826749028e163b0fa0e95370311
artryan130 <80287857+artryan130@users.noreply.github.com>
2021-11-27 11:28:50 -0300

    Editar imovel

--- src/views/dashboard/editImovel/EditImovel.jsx
+++ src/views/dashboard/editImovel/EditImovel.jsx
@@ -2,8 +2,30 @@ import './EditImovel.css'
 
 export default function EditImovel() {
     return(
-        <div>
-
-        </div>
+        <div className="Edit">
+            <div className="title">
+                <h1>Editar Imóvel</h1>
+            </div>
+                <form className="form">
+                    <h2>*Preenchimento obrigatório</h2>
+                        <div className="externa">
+                            <div className="line1">
+                                <input className="nome" type="text" required placeholder="Vamos importar os dados*"/>
+                            </div>
+                            <div className="line2">
+                                <input className="endereco" type="text" required />
+                            </div>
+                            <div className="line3">
+                                <input className="tipo" type="text" required />
+                                <input className="preco" type="text" required />
+                            </div>
+                            <div className="line4">
+                                <input className="url" type="text" />
+                                <input className="telefone" type="text" />
+                            </div>
+                        </div>
+        
+                    </form>
+            </div>
     )
 }
